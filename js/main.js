c2701afdd822b603bcdfa065e422f1a8acbaeab9
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-19 15:44:51 +0200

    start operations

--- js/main.js
+++ js/main.js
@@ -5,10 +5,24 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            current: ``,
         };
     },
     methods: {
-
+        clear() {
+            this.current = ``;
+        },
+        percent() {
+            this.current = this.current / 100;
+        },
+        addnumber(number) {
+            this.current = this.current + number;
+        },
+        dot() {
+            //Se non esiste `.` allora aggiungilo
+            if (this.current.indexOf(`.`) === -1) {
+                this.addnumber(`.`);
+            }
+        }
     },
 }).mount('#app');
